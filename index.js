6dbb4328680f9de8f7f45c2087d7fefde1a20ed9
Beka Zandukeli <beka.zandukeli@holovis.com>
2022-09-12 00:20:32 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./queries');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.port | 3001;
+
+app.use(cors({}));
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' });
+});
+
+app.get('/users', db.getUsers);
+app.get('/users/:id', db.getUserById);
+app.post('/users', db.createUser);
+app.put('/users/:id', db.updateUser);
+app.delete('/users/:id', db.deleteUser);
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
