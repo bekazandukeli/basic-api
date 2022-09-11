ce2bf75d4c546b06a444bbf1bfa2da13ecbcf5c6
Beka Zandukeli <beka.zandukeli@holovis.com>
2022-09-12 02:00:43 +0300

    fixed env port typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const db = require('./queries');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.port | 3001;
+const port = process.env.PORT | 3001;
 
 app.use(cors({}));
 app.use(express.json());
